0bb9734e6b8fd21a2b5d9aa9602217d999efa28f
Raouf Rokhjavan <rokhjavan.r@gmail.com>
2017-04-26 21:26:22 +0430

    - Add _cleanup() to remove created objects during test in the end - Add _fail() to handle state of failure - Add _inform() for notification and logging - Improve failure handling and logging   ($TEST_RESULT_DIR/{log,succesful_tests,failed_tests}) - Modify fsck.f2fs to report inconsistency of file system - Modify replay-log to report fsck_script failure and pass replay-entry   as environment variable to fsck_script - Add _log() to facilitate logging

--- fsck_scripts/fsck_inconsistent.sh
+++ fsck_scripts/fsck_inconsistent.sh
@@ -1,24 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 
 . config
+. utils/log
 
-trap "_fail 'fsck_inconsistent cancelled!'; exit 1;" 2 15
+trap "_fail 'fsck_inconsistent cancelled!'; exit -1;" 2 15
 _fail()
 {
-	echo $1
+	_log $1
 }
 
 # Check the consistency of file system
 [ $_DEBUG == "OFF" ] && FSCK_DEBUG="> /dev/null 2>&1"
 
-eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $REPLAYDEV "${FSCK_DEBUG}" \
-	|| _fail "fsck failed at entry $ENTRY"
-mount -t ${FSTYPE} $REPLAYDEV $CCTESTS_MNT || _fail "mount failed"
-echo
-umount $CCTESTS_MNT
+eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $REPLAYDEV "${FSCK_DEBUG}"	||\
+	{ _fail "fsck failed at entry $ENTRY_NUM."; exit -1; }
+
+mount -t ${FSTYPE} $REPLAYDEV $CCTESTS_MNT ||\
+	{ _fail "mount failed at entry $ENTRY_NUM."; exit -1; }
+
+umount $CCTESTS_MNT &> /dev/null
 if [ $? -ne 0 ]; then
 	sleep 1
-	umount $CCTESTS_MNT
+	umount $CCTESTS_MNT ||\
+	{ _fail "umount failed at entry $ENTRY_NUM."; exit -1; }
 fi
-eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $REPLAYDEV "${FSCK_DEBUG}" \
-	|| _fail "fsck failed"
+
+eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $REPLAYDEV "${FSCK_DEBUG}" ||\
+	{ _fail "fsck failed at entry $ENTRY_NUM."; exit -1; }
--- fsck_scripts/fsck_snap.sh
+++ fsck_scripts/fsck_snap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 . config
+. utils/log
 
-trap "_fail 'fsck_snap cancelled!'; exit 1;" 2 15
+trap "_fail 'fsck_snap cancelled!'; exit -1;" 2 15
 
 _fail()
 {
-	echo $1
-	umount $TARGET 2> /dev/null
+	_log $1
+	umount $TARGET &> /dev/null
 	if [ -b $SNAPSHOTBASE_DEV ]; then
 		dmsetup remove -f $SNAPSHOTCOW
 		dmsetup remove -f $SNAPSHOTBASE
@@ -24,43 +25,47 @@ then
 	# again.
 	sleep 1
 	dmsetup create $SNAPSHOTBASE --table "$ORIGIN_TABLE" || \
-		_fail "Couldn't dmsetup"
+		{ _fail "Creating snapshot-origin target failed at entry $ENTRY_NUM."; exit -1; }
 fi
 
 dmsetup create $SNAPSHOTCOW --table "$COW_TABLE" 
 if [ $? -ne 0 ]; then
 	sleep 1
 	dmsetup create $SNAPSHOTCOW --table "$COW_TABLE" || \
-			_fail "failed to create snapshot"
+		{ _fail "Creating snapshot target failed at entry $ENTRY_NUM."; exit -1; }
 fi
 
 # Check the consistency of file system
 [ $_DEBUG == "OFF" ] && FSCK_DEBUG="> /dev/null 2>&1"
 
-eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $TARGET "${FSCK_DEBUG}" \
-	|| _fail "fsck failed at entry $ENTRY"
-mount -t ${FSTYPE} $TARGET $CCTESTS_MNT || _fail "mount failed"
+eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $TARGET "${FSCK_DEBUG}" ||\
+	{ _fail "fsck failed at entry $ENTRY_NUM."; exit -1; }
 
-umount $CCTESTS_MNT
+mount -t ${FSTYPE} $TARGET $CCTESTS_MNT ||\
+	{ _fail "mount failed at entry $ENTRY_NUM."; exit -1; }
+
+umount $CCTESTS_MNT &> /dev/null
 if [ $? -ne 0 ]; then
 	sleep 1
-	umount $CCTESTS_MNT || _fail "umount failed."
+	umount $CCTESTS_MNT &> /dev/null ||\
+	{ _fail "umount failed at entry $ENTRY_NUM."; exit -1; }
 fi
-eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $TARGET "${FSCK_DEBUG}" \
-	|| _fail "fsck failed"
+
+eval ${TOOLS_DIR}/${FSCK} $FSCK_OPTS $TARGET "${FSCK_DEBUG}" ||\
+	{ _fail "fsck failed at entry $ENTRY_NUM."; exit -1; }
 
 # Remove snapshot* targets
 dmsetup remove -f $SNAPSHOTCOW
 if [ $? -ne 0 ]; then
 	sleep 1
 	dmsetup remove $SNAPSHOTCOW  || \
-		_fail "Couldn't dmsetup"
+	{ _fail "Removing snapshot target failed at entry $ENTRY_NUM."; exit -1; }
 fi
 
 dmsetup remove -f $SNAPSHOTBASE
 if [ $? -ne 0 ]; then
 	sleep 1
 	dmsetup remove $SNAPSHOTBASE  || \
-		_fail "Couldn't dmsetup"
+	{ _fail "Removing snapshot-origing failed at entry $ENTRY_NUM."; exit -1; }
 fi
 
